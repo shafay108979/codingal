b738460a50533a92b74e46b93948395c0c00e487
shafay108979 <muhammadshafaye63@gmail.com>
2025-07-10 21:25:45 +0500

    lesson19

---
+++ lesson19/script.js
@@ -0,0 +1,11 @@
+// this is a comment
+var z = 10
+var y = 18
+var a = 167
+var b = "shafay"
+var d = true
+var d = "mercedes"
+var ankushgupta = {printhisage: 24}
+
+console.log(a)
+console.log(ankushgupta.printhisage)
