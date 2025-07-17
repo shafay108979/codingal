51fb879ee6a9da80bbba391ae344051a512fe39a
shafay108979 <muhammadshafaye63@gmail.com>
2025-07-17 21:27:15 +0500

    lesson21

---
+++ lesson21/script.js
@@ -0,0 +1,15 @@
+Math.random()
+
+var a = 'shafay'
+
+console.log(a.toUpperCase())
+console.log(a.startsWith("s"))
+
+var b = 18.45
+
+console.log(Math.floor(b))
+
+let is
+for(i=0; i<100; i++){
+    console.log("Shafay")
+}
