aafc0abcb505698f4e10cc2e3140c5f09de13c85
shafay108979 <muhammadshafaye63@gmail.com>
2025-07-28 21:07:14 +0500

    lesson23

---
+++ lesson23/script.js
@@ -0,0 +1,15 @@
+var a = 'ritwika and shafay'
+var b = 80
+var c = [80, 90, 100, 120, 160] 
+var d = ['shafay', 'pinky', 'riya', 'aarti']
+
+console.log(d[0])
+console.log(c[2])
+
+console.log(c)
+
+var e = c.join(" $ ")
+console.log(e)
+
+c.push(680)
+console.log(c)
