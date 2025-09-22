8d45b34edb1a20d0165a887432b55a2d9e3b11fc
shafay108979 <muhammadshafaye63@gmail.com>
2025-09-22 21:32:18 +0500

    lesson2p

---
+++ lesson2p/demo.py
@@ -0,0 +1,39 @@
+a = 108
+b = 42.43323456
+c = 'i am a student of ankush gupta'
+d = [1, 2, 3, 4, 5]
+e = True
+f = False
+
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(a))
+
+a = str(a)
+b = int(b)
+c = str(c)
+d = str(d)
+e = float(e)
+f = int(f)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+a = int(a)
+b = float(b)
+
+res = 54 + a / b * 43 - 2 % 4 // 35 ** 2
+print(res)
+
+print(15 % 25)
+print(15 // 25)
+print(15 / 25) #floor division
+
+z = input('enter your name: ')
+
+print(f'hello {z}')
